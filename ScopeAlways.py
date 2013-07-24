38c412f4f791dee10f09d1315e8c65ec23cdaf97
William Yaworsky <wj.px01@gmail.com>
2013-07-24 08:52:18 -0400

    reworked settings loading to compensate for async loading of st3. it now works as it should

--- ScopeAlways.py
+++ ScopeAlways.py
@@ -2,19 +2,20 @@ import sublime, sublime_plugin
 
 STATUS_FORMAT = '%s'
 
-def init():
-  settings.add_on_change('extensions_path', update_settings)
+settings = None
+
+def plugin_loaded():
   update_settings()
+  settings.add_on_change('extensions_path', update_settings)
 
 def update_settings():
-  self.settings = sublime.load_settings('ScopeAlways.sublime-settings')
+  globals()['settings'] = sublime.load_settings('ScopeAlways.sublime-settings')
 
 class ScopeAlways(sublime_plugin.EventListener):
 
   def on_selection_modified(self, view):
     position = view.sel()[0].begin()
     scope    = view.scope_name(position)
-    key      = self.settings.get('status_key', 'scope_always')
+    key      = globals()['settings'].get('status_key', 'scope_always')
     view.set_status(key, STATUS_FORMAT % scope)
 
-init()
