a87a8218cafcf1d304f27aef73f997d3456719f5
William Yaworsky <wj.px01@gmail.com>
2013-07-24 08:31:06 -0400

    removed status_format from settings and changed how settings are loaded

--- ScopeAlways.py
+++ ScopeAlways.py
@@ -1,12 +1,20 @@
 import sublime, sublime_plugin
 
-settings = sublime.load_settings('ScopeAlways.sublime-settings')
+STATUS_FORMAT = '%s'
+
+def init():
+  settings.add_on_change('extensions_path', update_settings)
+  update_settings()
+
+def update_settings():
+  self.settings = sublime.load_settings('ScopeAlways.sublime-settings')
 
 class ScopeAlways(sublime_plugin.EventListener):
 
   def on_selection_modified(self, view):
     position = view.sel()[0].begin()
     scope    = view.scope_name(position)
-    key      = settings.get('status_key', 'scope_always')
-    format   = settings.get('status_format', '%s')
-    view.set_status(key, format % scope)
+    key      = self.settings.get('status_key', 'scope_always')
+    view.set_status(key, STATUS_FORMAT % scope)
+
+init()
