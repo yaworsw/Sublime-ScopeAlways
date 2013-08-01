f2199bb2acd2fc63c78484ca61abee4103e6a0ec
William Yaworsky <wj.px01@gmail.com>
2013-08-01 05:45:22 -0400

    changed how globals are accessed
    instead of using globals() to access globals as a dict globals are now
    just inferred where appropriate

--- ScopeAlways.py
+++ ScopeAlways.py
@@ -8,13 +8,13 @@ settings = None
 def plugin_loaded():
   update_settings()
   settings.add_on_change('extensions_path', update_settings)
-  globals()['on'] = settings.get('start_on')
+  on = settings.get('start_on')
 
 def update_settings():
   globals()['settings'] = sublime.load_settings('ScopeAlways.sublime-settings')
 
 def show_scope(view, hide=False):
-  key = globals()['settings'].get('status_key', 'scope_always')
+  key = settings.get('status_key', 'scope_always')
   if not hide:
     position = view.sel()[0].begin()
     scope    = view.scope_name(position)
@@ -33,5 +33,6 @@ class ScopeAlways(sublime_plugin.EventListener):
 class ToggleScopeAlways(sublime_plugin.WindowCommand):
 
   def run(self):
-    on = globals()['on'] = not globals()['on']
+    global on
+    on = not on
     show_scope(self.window.active_view(), hide=not on)
